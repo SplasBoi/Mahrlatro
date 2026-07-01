b1ca02194e2e52808e337b6b0460e58380630834
SplasBoi <102168976+SplasBoi@users.noreply.github.com>
2026-07-01 18:25:03 +0200

    fix broken mimahr joker

--- jokers/mimahr.lua
+++ jokers/mimahr.lua
@@ -28,8 +28,8 @@ SMODS.Joker { --Mimahr
     atlas = 'CustomJokers',
     
     calculate = function(self, card, context)
-        if context.repetition and context.cardarea == G.play  then
-            if context.other_card.seal == "mahrlatr_mahrseal" then
+        if context.repetition and context.cardarea == G.play then
+            if context.other_card.seal == "mahrlatr_mahr" then
                 return {
                     repetitions = card.ability.extra.repetitions,
                     message = localize('k_again_ex')
