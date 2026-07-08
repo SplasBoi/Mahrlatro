465fb89e7ba13e9ab3912daa389fe48b9e90a38b
SplasBoi <102168976+SplasBoi@users.noreply.github.com>
2026-07-08 21:48:45 +0200

    fix where_mahr counting cards incorrectly

--- jokers/where_mahr.lua
+++ jokers/where_mahr.lua
@@ -1,23 +1,39 @@
+local get_xmult = nil
+
 SMODS.Joker {
     key = "where_mahr",
     blueprint_compat = true,
     rarity = 2,
     cost = 8,
-    pos = { x = 4, y = 7 },
+    pos = {
+        x = 4,
+        y = 7
+    },
 
     unlocked = true,
     discovered = true,
     atlas = 'CustomJokers',
 
     loc_vars = function(self, info_queue, card)
-        return { vars = { G.jokers and math.max(1, (G.jokers.config.card_limit - #G.jokers.cards) + #SMODS.find_card("j_vremade_stencil", true)) or 1 } }
+        return {
+            vars = {
+                get_xmult()
+            }
+        }
     end,
     calculate = function(self, card, context)
         if context.joker_main then
             return {
-                xmult = math.max(1,
-                    (G.jokers.config.card_limit - #G.jokers.cards) + #SMODS.find_card("j_vremade_stencil", true))
+                xmult = get_xmult()
             }
         end
     end
 }
+
+get_xmult = function()
+    if G.jokers then
+        return math.max(1, (G.jokers.config.card_limit - #G.jokers.cards) + 1)
+    else
+        return 1
+    end
+end
