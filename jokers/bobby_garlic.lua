55af91c3bc1eabe3d3ba4f0d246900b259cba18a
SplasBoi <102168976+SplasBoi@users.noreply.github.com>
2026-07-15 22:47:12 +0200

    bobby garlic code duplication reduction

--- jokers/bobby_garlic.lua
+++ jokers/bobby_garlic.lua
@@ -1,3 +1,5 @@
+local set_random_poker_hand = nil
+
 SMODS.Joker {
     key = "bobby_garlic",
 
@@ -32,44 +34,39 @@ SMODS.Joker {
     end,
 
     add_to_deck = function(self, card, from_debuff)
-        local _poker_hands = {}
-
-        for handname, _ in pairs(G.GAME.hands) do
-            if SMODS.is_poker_hand_visible(handname) and handname ~= card.ability.extra.poker_hand then
-                _poker_hands[#_poker_hands + 1] = handname
-                play_sound('mahrlatr_bobby_try_this_hand')
-            end
-        end
-
-        card.ability.extra.poker_hand = pseudorandom_element(_poker_hands, 'j_mahrlatr_bobby_garlic')
+        card.ability.extra.poker_hand = get_random_poker_hand(card)
+        play_sound("mahrlatr_bobby_try_this_hand")
     end,
     
     calculate = function(self, card, context)
-        if context.joker_main then
-            if context.scoring_name == card.ability.extra.poker_hand then
-                return {
-                    x_mult = card.ability.extra.x_mult,
-                    message = localize('bobby_good_job'),
-                    play_sound('mahrlatr_bobby_good_job')
-                }
-            end
+        if context.joker_main and context.scoring_name == card.ability.extra.poker_hand then
+            return {
+                x_mult = card.ability.extra.x_mult,
+                message = localize('bobby_good_job'),
+                sound = "mahrlatr_bobby_good_job"
+            }
         end
 
-        if context.end_of_round and context.game_over == false and context.main_eval and not context.blueprint then
-            local _poker_hands = {}
-
-            for handname, _ in pairs(G.GAME.hands) do
-                if SMODS.is_poker_hand_visible(handname) and handname ~= card.ability.extra.poker_hand then
-                    _poker_hands[#_poker_hands + 1] = handname
-                end
-            end
-
-            card.ability.extra.poker_hand = pseudorandom_element(_poker_hands, 'j_mahrlatr_bobby_garlic')
+        if context.end_of_round and not context.game_over and context.main_eval and not context.blueprint then
+            card.ability.extra.poker_hand = get_random_poker_hand(card)
 
             return {
-                play_sound('mahrlatr_bobby_try_this_hand'),
-                message = localize('bobby_try_this_hand')
+                message = localize('bobby_try_this_hand'),
+                sound = "mahrlatr_bobby_try_this_hand"
             }
         end
     end
 }
+
+get_random_poker_hand = function(card)
+    local current_hand = card.ability.extra.poker_hand or ""
+    local poker_hands = {}
+
+    for handname, _ in pairs(G.GAME.hands) do
+        if SMODS.is_poker_hand_visible(handname) and handname ~= current_hand then
+            table.insert(poker_hands, handname)
+        end
+    end
+
+    return pseudorandom_element(poker_hands, card.config.center_key)
+end
