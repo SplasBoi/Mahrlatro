107f70bb30eeb80d5dbba2f0f88058298832a4a4
SplasBoi <102168976+SplasBoi@users.noreply.github.com>
2026-07-18 23:43:33 +0200

    wayne joker destroy logic

--- jokers/wayne.lua
+++ jokers/wayne.lua
@@ -1,5 +1,8 @@
+local add_double_tag = nil
+
 SMODS.Joker {
     key = "wayne",
+
     blueprint_compat = true,
     eternal_compat = false,
     unlocked = true,
@@ -11,17 +14,19 @@ SMODS.Joker {
 
     config = {
         extra = {
-            numerator = 1,
-            denominator = 2
+            base_numerator = 1,
+            base_denominator = 2
         }
     },
 
     loc_vars = function(self, info_queue, card)
-        local num, denom = SMODS.get_probability_vars(card, card.ability.extra.numerator, card.ability.extra.denominator)
+        local e = self.config.extra or card.ability.extra
+
+        local num, denom = SMODS.get_probability_vars(card, e.base_numerator, e.base_denominator)
 
         return {
             vars = {
-                colours = { HEX('000000') },
+                colours = { HEX('000000')},
 
                 num,
                 denom
@@ -29,20 +34,29 @@ SMODS.Joker {
         }
     end,
 
-
     calculate = function(self, card, context)
-        if context.end_of_round and context.game_over == false and context.main_eval then
-            G.E_MANAGER:add_event(Event({
-                func = (function()
-                    add_tag({ key = 'tag_double' })
-                    play_sound('generic1', 0.9 + math.random() * 0.1, 0.8)
-                    play_sound('holo1', 1.2 + math.random() * 0.1, 0.4)
-                    return true
-                end)
-            }))
-            return nil, true
+        local e = self.config.extra or card.ability.extra
+
+        if context.end_of_round and not context.game_over and context.main_eval then
+            local num, denom = SMODS.get_probability_vars(card, e.base_numerator, e.base_denominator)
+
+            add_double_tag()
+
+            if SMODS.pseudorandom_probability(card, card.config.center_key, num, denom) then
+                local joker_to_destroy = G.jokers.cards[math.random(#G.jokers.cards)]
+                SMODS.destroy_cards(joker_to_destroy)
+            end
         end
     end
-
-    -- i cannot figure out how to make this guy destroy a joker 😭
 }
+
+add_double_tag = function()
+    G.E_MANAGER:add_event(Event({
+        func = function()
+            add_tag({ key = 'tag_double' })
+            play_sound('generic1', 0.9 + math.random() * 0.1, 0.8)
+            play_sound('holo1', 1.2 + math.random() * 0.1, 0.4)
+            return true
+        end
+    }))
+end
