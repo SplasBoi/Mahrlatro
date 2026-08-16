d2742dbc1a9c11846ece20459ff6e82c68a84c1d
flyingdragon <marco14963@gmail.com>
2026-08-16 14:53:36 -0300

    Fixed Face Monstahr for good (hopefully)
    Also made it uncommon instead of common. Too strong for a Common

--- jokers/face_monstahr.lua
+++ jokers/face_monstahr.lua
@@ -1,6 +1,3 @@
-local add_tarot_card = nil
-local free_consumeable_slots = nil
-
 SMODS.Joker {
     key = "face_monstahr",
 
@@ -23,7 +20,7 @@ SMODS.Joker {
     pools = { ["mahrlatr_mahrlatr_jokers"] = true },
 
     cost = 5,
-    rarity = 1,
+    rarity = 2,
 
     config = {
         extra = {
@@ -47,38 +44,26 @@ SMODS.Joker {
     calculate = function(self, card, context)
         local e = self.config.extra or card.ability.extra
 
-        if context.discard and context.other_card:is_face() then
-            if SMODS.pseudorandom_probability(card, self.key, e.numerator, e.denominator) then
-                if free_consumeable_slots() > 0 then
-                    return {
-                        extra = {
-                            message = localize('k_plus_tarot'),
-                            message_card = card,
-                            func = add_tarot_card(self)
-                        }
-                    }
-                end
-            end
+        if context.discard and #G.consumeables.cards + G.GAME.consumeable_buffer < G.consumeables.config.card_limit then
+            G.GAME.consumeable_buffer = G.GAME.consumeable_buffer + 1
+            G.E_MANAGER:add_event(Event({
+                func = (function()
+                    G.E_MANAGER:add_event(Event({
+                        func = function()
+                            SMODS.add_card {
+                                set = 'Tarot',
+                                key_append = 'j_mahrlatr_face_monstahr'
+                            }
+                            G.GAME.consumeable_buffer = 0
+                            return true
+                        end
+                    }))
+                    SMODS.calculate_effect({ message = localize('k_plus_tarot'), colour = G.C.PURPLE },
+                        context.blueprint_card or card)
+                    return true
+                end)
+            }))
+            return nil, true -- This is for Joker retrigger purposes
         end
     end
 }
-
-add_tarot_card = function(self)
-    G.E_MANAGER:add_event(Event({
-        trigger = "immediate",
-        func = function()
-            SMODS.add_card {
-                set = 'Tarot',
-                key_append = self.key
-            }
-            return true
-        end
-    }))
-end
-
-free_consumeable_slots = function()
-    local consumeables = #G.consumeables.cards
-    local consumeable_limit = G.consumeables.config.card_limit
-
-    return math.max(0, consumeable_limit - consumeables)
-end
