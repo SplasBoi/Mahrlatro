8fa68b7de166a0a5a6e2c6e627c6c6c78675054b
SplasBoi <102168976+SplasBoi@users.noreply.github.com>
2026-08-16 18:26:25 +0200

    face monster tarot overflow fix

--- jokers/face_monstahr.lua
+++ jokers/face_monstahr.lua
@@ -1,3 +1,6 @@
+local add_tarot_card = nil
+local free_consumeable_slots = nil
+
 SMODS.Joker {
     key = "face_monstahr",
 
@@ -30,7 +33,8 @@ SMODS.Joker {
     },
 
     loc_vars = function(self, info_queue, card)
-        local num, denom = SMODS.get_probability_vars(card, card.ability.extra.numerator, card.ability.extra.denominator)
+        local e = self.config.extra or card.ability.extra
+        local num, denom = SMODS.get_probability_vars(card, e.numerator, e.denominator)
 
         return {
             vars = {
@@ -41,29 +45,40 @@ SMODS.Joker {
     end,
 
     calculate = function(self, card, context)
+        local e = self.config.extra or card.ability.extra
+
         if context.discard and context.other_card:is_face() then
-            if SMODS.pseudorandom_probability(card, 'j_mahrlatr_face_monstahr', card.ability.extra.numerator, card.ability.extra.denominator) then
-                return {
-                    extra = {
-                        message = localize('k_plus_tarot'),
-                        message_card = card,
-                        func = (function()
-                            G.E_MANAGER:add_event(Event({
-                                func = (function()
-                                    if #G.consumeables.cards < G.consumeables.config.card_limit then
-                                        SMODS.add_card {
-                                            set = 'Tarot',
-                                            key_append = 'j_mahrlatr_face_monstahr'
-                                        }
-                                    end
-                                    
-                                    return true
-                                end)
-                            }))
-                        end)
+            if SMODS.pseudorandom_probability(card, self.key, e.numerator, e.denominator) then
+                if free_consumeable_slots() > 0 then
+                    return {
+                        extra = {
+                            message = localize('k_plus_tarot'),
+                            message_card = card,
+                            func = add_tarot_card(self)
+                        }
                     }
-                }
+                end
             end
         end
     end
 }
+
+add_tarot_card = function(self)
+    G.E_MANAGER:add_event(Event({
+        trigger = "immediate",
+        func = function()
+            SMODS.add_card {
+                set = 'Tarot',
+                key_append = self.key
+            }
+            return true
+        end
+    }))
+end
+
+free_consumeable_slots = function()
+    local consumeables = #G.consumeables.cards
+    local consumeable_limit = G.consumeables.config.card_limit
+
+    return math.max(0, consumeable_limit - consumeables)
+end
