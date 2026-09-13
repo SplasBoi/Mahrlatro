7a260af09e87f3d2639933890b76aeb64f066d55
SplasBoi <102168976+SplasBoi@users.noreply.github.com>
2026-09-13 23:46:36 +0200

    cheese jokerDisplay

--- jokers/cheese.lua
+++ jokers/cheese.lua
@@ -11,32 +11,60 @@ SMODS.Joker {
     atlas = 'CustomJokers',
     pools = { ["mahrlatr_mahrlatr_jokers"] = true },
 
-    config = { extra = { chips = 5, chip_mod = 5 } },
+    config = {
+        extra = {
+            chips = 5,
+            chip_mod = 5
+        }
+    },
 
     loc_vars = function(self, info_queue, card)
         return { vars = { card.ability.extra.chips, card.ability.extra.chip_mod } }
     end,
 
     calculate = function(self, card, context)
+        local e = card.ability.extra or self.config.extra
+
+        if context.joker_main then
+            return {
+                chips = e.chips
+            }
+        end
+
         if context.after and not context.blueprint then
-            if card.ability.extra.chips + card.ability.extra.chip_mod >= 100 then
+            e.chips = e.chips + e.chip_mod
+            if e.chips >= 100 then
                 SMODS.destroy_cards(card, nil, nil, true)
                 return {
                     message = localize('gone_bad'),
                     colour = G.C.CHIPS
                 }
             else
-                card.ability.extra.chips = card.ability.extra.chips + card.ability.extra.chip_mod
                 return {
                     message = '+5 chips',
                     colour = G.C.CHIPS
                 }
             end
         end
-        if context.joker_main then
-            return {
-                chips = card.ability.extra.chips
-            }
-        end
+    end,
+
+    joker_display_def = function(JokerDisplay)
+        return {
+            text = {
+                { text = "+" },
+                {
+                    ref_table = "card.joker_display_values",
+                    ref_value = "chips"
+                }
+            },
+            text_config = { colour = G.C.CHIPS },
+            calc_function = function(card)
+                local e = card.ability.extra
+
+                -- e.chips increases before hand finishes playing, so this is the best solution i can come up to
+                local chips = (G.STATE == G.STATES.HAND_PLAYED and e.chips - e.chip_mod) or e.chips or 0
+                card.joker_display_values.chips = chips
+            end
+        }
     end
 }
