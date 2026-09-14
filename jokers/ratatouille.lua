0cf95e36f3cf47f18e1a801a673e047a2fa0df01
SplasBoi <102168976+SplasBoi@users.noreply.github.com>
2026-09-14 17:08:00 +0200

    reduce sound on roffles_chad and ratatouille

--- jokers/ratatouille.lua
+++ jokers/ratatouille.lua
@@ -3,9 +3,13 @@ SMODS.Joker:take_ownership('j_jolly',
     pos = { x = 3, y = 13 },
     atlas = 'CustomJokers',
     pools = { ["mahrlatr_mahrlatr_jokers"] = true },
+
+    add_to_deck = function(self, card, from_debuff)
+        SoundUtility.play_sound_if_exists('mahrlatr_ratatouille')
+    end,
+
     calculate = function(self, card, context)
         if context.joker_main and next(context.poker_hands['Pair']) then
-            SoundUtility.play_sound_if_exists('mahrlatr_ratatouille')
             return {
                 mult = 8
             }
--- jokers/roffles_chad.lua
+++ jokers/roffles_chad.lua
@@ -2,11 +2,15 @@ SMODS.Joker:take_ownership('j_hanging_chad',
     { -- table of properties to change from the existing object
     pos = { x = 5, y = 17 },
     atlas = 'CustomJokers',
+
+    add_to_deck = function(self, card, from_debuff)
+        SoundUtility.play_sound_if_exists("mahrlatr_roffle")
+    end,
+
     calculate = function(self, card, context)
         if context.repetition and context.cardarea == G.play and context.other_card == context.scoring_hand[1] then
             return {
                 repetitions = 2,
-                SoundUtility.play_sound_if_exists('mahrlatr_roffle')
             }
         end
     end,
