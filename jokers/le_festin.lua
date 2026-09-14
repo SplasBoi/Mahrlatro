7b54b338ef15c40d60eb4e9a30b190ad3ac93f23
SplasBoi <102168976+SplasBoi@users.noreply.github.com>
2026-09-14 12:31:03 +0200

    le festin jokerDisplay

--- jokers/le_festin.lua
+++ jokers/le_festin.lua
@@ -1,4 +1,6 @@
-local is_unique = nil
+local function is_unique(joker_key, already_owned)
+    return not TableUtility.contains_individual(joker_key, already_owned)
+end
 
 SMODS.Joker {
     key = "le_festin",
@@ -46,22 +48,40 @@ SMODS.Joker {
     calculate = function(self, card, context)
         local e = card.ability.extra
 
+        if context.joker_main then
+            return {
+                mult = e.mult
+            }
+        end
+
         if context.buying_card then
+            local card_key = context.card.config.center.key
             if (context.card.config.center.pools or {}).mahrlatr_food_jokahr then
-                if not is_unique(context.card.config.center.key, e.already_owned) then return end
+                if not is_unique(card_key, e.already_owned) then
+                    return
+                end
 
                 SMODS.scale_card(card, {
-                    ref_table = card.ability.extra,
-                    ref_value = 'mult',
-                    scalar_value = 'scaling'
+                    ref_table = e,
+                    ref_value = "mult",
+                    scalar_value = "scaling"
                 })
                 
-                table.insert(e.already_owned, context.card.config.center.key)
+                table.insert(e.already_owned, card_key)
             end
         end
+    end,
+
+    joker_display_def = function(JokerDisplay)
+        return {
+            text = {
+                { text = "+" },
+                {
+                    ref_table = "card.ability.extra",
+                    ref_value = "mult",
+                }
+            },
+            text_config = { colour = G.C.RED }
+        }
     end
 }
-
-is_unique = function(joker_key, already_owned)
-    return not TableUtility.contains_individual(joker_key, already_owned)
-end
