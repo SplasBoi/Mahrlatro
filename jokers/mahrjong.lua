e538df7cb42f81a5bec555bf8a2ae6dfdf3fb965
SplasBoi <102168976+SplasBoi@users.noreply.github.com>
2026-09-14 13:33:53 +0200

    mahrjong jokerDisplay

--- jokers/mahrjong.lua
+++ jokers/mahrjong.lua
@@ -1,6 +1,35 @@
-local has_three_of_a_kind = nil
-local has_sequence = nil
-local sort_held_hand = nil
+local function has_three_of_a_kind(cards)
+    for i = 1, #cards - 2 do
+        if cards[i] == cards[i + 1] and cards[i + 1] == cards[i + 2] then
+            return true
+        end
+    end
+    
+    return false
+end
+
+local function has_sequence(cards)
+    for i = 1, #cards - 2 do
+        if cards[i] == cards[i + 1] + 1
+        and cards[i + 1] == cards[i + 2] + 1 then
+            return true
+        end
+    end
+
+    return false
+end
+
+local function sort_held_hand(cards)
+    local held_cards = {}
+
+    for _, playing_card in ipairs(cards) do
+        table.insert(held_cards, playing_card:get_id())
+    end
+
+    table.sort(held_cards, function(a, b) return a > b end)
+    
+    return held_cards
+end
 
 SMODS.Joker {
     key = "mahrjong",
@@ -8,7 +37,12 @@ SMODS.Joker {
     discovered = false,
     unlocked = true,
     atlas = 'CustomJokers',
-    pos = {x = 6, y = 9},
+
+    pos = {
+        x = 6,
+        y = 9
+    },
+
     pools = { ["mahrlatr_mahrlatr_jokers"] = true },
 
     rarity = 2,
@@ -42,38 +76,34 @@ SMODS.Joker {
                 }
             end
         end
+    end,
+
+    joker_display_def = function(JokerDisplay)
+        return {
+            text = {
+                {
+                    border_nodes = {
+                        { text = "X" },
+                        {
+                            ref_table = "card.ability.extra",
+                            ref_value = "x_mult",
+                            retrigger_type = "exp"
+                        }
+                    }
+                }
+            },
+            
+            calc_function = function(card)
+                local e = card.ability.extra
+                
+                local sorted_held_hand = sort_held_hand(G.hand.cards)
+
+                if has_three_of_a_kind(sorted_held_hand) or has_sequence(sorted_held_hand) then
+                    card.joker_display_values.x_mult = e.x_mult
+                else
+                    card.joker_display_values.x_mult = 1
+                end
+            end
+        }
     end
 }
-
-has_three_of_a_kind = function(cards)
-    for i = 1, #cards - 2 do
-        if cards[i] == cards[i + 1] and cards[i + 1] == cards[i + 2] then
-            return true
-        end
-    end
-    
-    return false
-end
-
-has_sequence = function(cards)
-    for i = 1, #cards - 2 do
-        if cards[i] == cards[i + 1] + 1
-        and cards[i + 1] == cards[i + 2] + 1 then
-            return true
-        end
-    end
-
-    return false
-end
-
-sort_held_hand = function(cards)
-    local held_cards = {}
-
-    for _, playing_card in ipairs(cards) do
-        table.insert(held_cards, playing_card:get_id())
-    end
-
-    table.sort(held_cards, function(a, b) return a > b end)
-    
-    return held_cards
-end
