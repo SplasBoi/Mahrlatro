c2e9efe734387918020cb2b6fc6d5163e485021d
flyingdragon <marco14963@gmail.com>
2026-09-15 00:49:44 -0300

    JokerDisplay: Black Metahrl

--- jokers/black_metahrl.lua
+++ jokers/black_metahrl.lua
@@ -30,7 +30,7 @@ SMODS.Joker {
     },
 
     loc_vars = function(self, info_queue, card)
-        local num, denom = SMODS.get_probability_vars(card, card.ability.extra.numerator, card.ability.extra.denominator)
+        local num, denom = SMODS.get_probability_vars(card, card.ability.extra.numerator, card.ability.extra.denominator, 'black_metahrl')
 
         return {
             vars = {
@@ -67,5 +67,49 @@ SMODS.Joker {
                 }
             end
         end
+    end,
+
+    joker_display_def = function(JokerDisplay)
+        return {
+            text = {
+                { text = "+" },
+                { ref_table = "card.joker_display_values", ref_value = "count", retrigger_type = "mult" },
+            },
+
+            text_config = { colour = G.C.SECONDARY_SET.Tarot },
+
+            extra = {
+                {
+                    { text = "(" },
+                    { ref_table = "card.joker_display_values", ref_value = "odds" },
+                    { text = ")" },
+                }
+            },
+
+            extra_config = { colour = G.C.GREEN, scale = 0.3 },
+
+            calc_function = function(card)
+                local count = 0
+                local text, _, scoring_hand = JokerDisplay.evaluate_hand()
+
+                if text ~= 'Unknown' then
+                    for _, scoring_card in pairs(scoring_hand) do
+                        if SuitHelpers.is_black_card(scoring_card) then
+                            count = count + JokerDisplay.calculate_card_triggers(scoring_card, scoring_hand)
+                        end
+                    end
+                end
+
+                card.joker_display_values.count = count
+
+                local numerator, denominator = SMODS.get_probability_vars(card, card.ability.extra.numerator, card.ability.extra.denominator, 'black_metahrl')
+
+                card.joker_display_values.odds = localize { 
+                    type = 'variable',
+                    key = "jdis_odds",
+                    vars = { numerator, denominator }
+                }
+            end
+        }
     end
 }
