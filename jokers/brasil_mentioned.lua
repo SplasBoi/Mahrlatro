abc96971a2f72b39f7cd39788f6145eb911d5a50
flyingdragon <marco14963@gmail.com>
2026-09-15 07:17:02 -0300

    JokerDisplay: Brasil Mentioned

--- jokers/brasil_mentioned.lua
+++ jokers/brasil_mentioned.lua
@@ -83,5 +83,18 @@ SMODS.Joker {
                 chips = e.chips
             }
         end
+    end,
+
+    joker_display_def = function(JokerDisplay)
+        return {
+            text = {
+                { text = "+" },
+                {
+                    ref_table = "card.ability.extra",
+                    ref_value = "chips"
+                }
+            },
+            text_config = { colour = G.C.CHIPS },
+        }
     end
 }
